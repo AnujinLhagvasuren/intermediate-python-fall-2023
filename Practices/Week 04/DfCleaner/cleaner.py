112cee7e567219d791a967a2bd9c967f92304981
AnujinLhagvasuren <144761548+AnujinLhagvasuren@users.noreply.github.com>
2023-10-03 11:47:32 +0800

    g

---
+++ Practices/Week 04/DfCleaner/cleaner.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+class Cleaner:
+    def __init__(self,df):
+        self.df = df
+        
+    def drop_empty(self):
+        self.df = self.df.dropna()
+
+
+    def fill_empty(self, column_name):
+        self.df[column_name] = self.df[column_name].fillna(self.df[column_name].mean())
+
+    def drop_column(self, column_name):
+        self.df = self.df.drop(columns=column_name)
+
+
+    def fix_index(self):
+        self.df = self.df.reset_index(drop=True)
+
+
+    def fix_dates(self, column_name):
+        self.df.to_datetime(df[column_name])
+
